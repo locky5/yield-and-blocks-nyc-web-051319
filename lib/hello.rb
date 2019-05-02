579e7fb58f9d6db647c99c411987a1cfe03fd8d9
Kevin Dai <daikevin19@yahoo.com>
2019-05-02 03:22:59 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,7 @@ def hello_t(array)
   if block_given? ##returns true if method inside is called with a block
     i = 0 
     while i < array.length 
-      yield array[i] ##called with a block
+      yield array[i]
       i = i + 1 
     end
     array ##returns the original array since its the last line
