913db226b55ad4739c57ba90f8644e00939510ff
Kevin Dai <daikevin19@yahoo.com>
2019-05-02 02:39:05 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
-
+def hello_t(array)
+  if block_given? ##returns true if method inside is called with a block
+    i = 0 
+    while i < array.length 
+      yield array[i] ##called with a block
+      i = i + 1 
+    end
+    array ##returns the original array since its the last line
+  else
+    puts "Hey! No block was given!"
+  end
 end
-
-# call your method here!
-
